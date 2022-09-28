bc74bfaa94d8242bc6914b5be3e020bb352ae1fd
althafvly <althafvly@gmail.com>
2022-09-28 11:23:42 +0900

    SystemUIGoogle: Re-evaluate system theme on UI mode change
    - Need for power menu to set accurate colors
    
    Change-Id: I05d41eaf8ea19ce3b6ce659d01da33cf55de3b7e
    
    * Adapt to JAVA code base by mixin with original commit
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.java
@@ -33,6 +33,7 @@ import com.android.systemui.flags.SystemPropertiesHelper;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.policy.ConfigurationController;
+import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.theme.ThemeOverlayApplier;
 import com.android.systemui.theme.ThemeOverlayController;
@@ -46,6 +47,16 @@ import javax.inject.Inject;
 public class ThemeOverlayControllerGoogle extends ThemeOverlayController {
     private final Resources resources;
     private final SystemPropertiesHelper systemProperties;
+
+    private final ConfigurationController darkConfigurationController;
+    private final ConfigurationListener darkConfigurationListener =
+            new ConfigurationListener() {
+                @Override
+                public void onUiModeChanged() {
+                    Log.d(TAG, "Re-applying theme on UI change");
+                    reevaluateSystemTheme(true /* forceReload */);
+                }
+            };
 
     @Inject
     public ThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher,
@@ -62,6 +73,7 @@ public class ThemeOverlayControllerGoogle extends ThemeOverlayController {
                 resources, wakefulnessLifecycle, configurationController);
         this.systemProperties = systemPropertiesHelper;
         this.resources = resources;
+        darkConfigurationController = configurationController;
         configurationController.addCallback(new ConfigurationController.ConfigurationListener() {
             @Override
             public void onThemeChanged() {
@@ -77,7 +89,13 @@ public class ThemeOverlayControllerGoogle extends ThemeOverlayController {
             Log.d("ThemeOverlayController", "Boot animation colors " + i + ": " + i2);
         }
     }
-
+    
+    @Override
+    public void start() {
+        super.start();
+        darkConfigurationController.addCallback(darkConfigurationListener);
+    }
+        
     public final void setBootColorSystemProps() {
         try {
             int[] bootColors = getBootColors();
